fb93ea5521e4464054dc5db4a9fefc1cf1544821
Fagner Sales <fs.fagnersales@gmail.com>
2023-07-04 09:31:13 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
-import { PostCard } from "@/components/post/card";
-import { PostInfo } from "@/components/post/info";
-import { PostTitle } from "@/components/post/title";
-import { allPosts } from "contentlayer/generated";
+import { PostCard } from '@/components/post/card'
+import { PostInfo } from '@/components/post/info'
+import { PostTitle } from '@/components/post/title'
+import { allPosts } from 'contentlayer/generated'
 
 async function getAllPosts() {
   const posts = allPosts.filter(post => post.published)
 
-  if (posts.length === 0) throw new Error("No posts available")
+  if (posts.length === 0) throw new Error('No posts available')
 
   return posts.sort((a, b) => {
     return b.index - a.index
@@ -18,14 +18,14 @@ export default async function Home() {
 
   return (
     <main className='min-h-screen flex flex-col py-6 px-8 container mx-auto'>
-      <div className="text-accent-full font-black text-8xl -tracking-[5px] mb-[128px]">
+      <div className='text-accent-full font-black text-8xl -tracking-[5px] mb-[128px]'>
         <h1>Fagner Sales Blog</h1>
       </div>
 
       <div>
-        <h1 className="text-2xl font-black text-accent-full">Recent Posts</h1>
+        <h1 className='text-2xl font-black text-accent-full'>Recent Posts</h1>
 
-        <div className="divide-y divide-gray-600/40">
+        <div className='divide-y divide-gray-600/40'>
           {posts.map((post, index) => (
             <PostCard key={index}>
               <PostTitle
