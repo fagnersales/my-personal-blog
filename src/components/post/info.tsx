4cb528872e81fa4bd56b51d5639a35a96f650d77
Fagner Sales <fs.fagnersales@gmail.com>
2023-07-04 09:41:00 -0300

    partially implement heart to posts

--- src/components/post/info.tsx
+++ src/components/post/info.tsx
@@ -1,19 +1,39 @@
+'use client'
+
+import { Heart } from 'lucide-react'
+import { useState } from 'react'
+
 interface PostInfoProps {
   postedAt: string
   views: number
 }
 
 const formatDate = (date: string) =>
-  new Date(date).toLocaleDateString("en-US", {
-    year: "numeric",
-    month: "long",
-    day: "numeric",
+  new Date(date).toLocaleDateString('en-US', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
   });
 
 export function PostInfo(props: PostInfoProps) {
+  const [heartsCounter, setHeartsCounter] = useState<number>(0)
+
+  const incrementHearts = () => {
+    setHeartsCounter(heartsCounter => heartsCounter + 1)
+  }
+
   return (
-    <div>
-      <h2 className="text-gray-600">{formatDate(props.postedAt)}</h2>
+    <div className='flex gap-2'>
+      <h2 className='text-gray-600'>{formatDate(props.postedAt)}</h2>
+      <p className='text-gray-600'>-</p>
+      <div className='flex gap-1 items-center max-w-fit'>
+        <button onClick={incrementHearts}>
+          <Heart
+            className='text-gray-600 hover:text-red-300/50'
+          />
+        </button>
+        <h3 className='text-gray-600'>{heartsCounter}</h3>
+      </div>
     </div>
   )
 }
