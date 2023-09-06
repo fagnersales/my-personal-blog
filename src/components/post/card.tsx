7aa2062b9ccd01850868dc2b33d68c381940d1e5
Fagner Sales <fs.fagnersales@gmail.com>
2023-09-06 17:54:00 -0300

    removed x padding from post info

--- src/components/post/card.tsx
+++ src/components/post/card.tsx
@@ -1,7 +1,7 @@
 import { PropsWithChildren } from 'react'
 
 export function PostCard({children}: PropsWithChildren) {
-  return <div className='p-2'>
+  return <div className='py-2'>
     {children}
   </div>
 }
